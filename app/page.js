12730a4848cf0023fb37070df74d311dfd8ac806
gathacom <gathacom2222@gmail.com>
2025-06-30 23:10:43 +0700

    metadata

--- app/page.js
+++ app/page.js
@@ -7,15 +7,6 @@ export default function Home() {
     <div className="flex w-full h-screen">
       <SideNavbar />
       <div>
-        <h1>Dashboard</h1>
-        {/* <h2 className="text-2xl font-bold text-center">Total Record</h2>
-        <div className="grid grid-cols-3 gap-4">
-          <CardRecord></CardRecord>
-          <CardRecord></CardRecord>
-          <CardRecord></CardRecord>
-          <CardRecord></CardRecord>
-          <CardRecord></CardRecord>
-        </div> */}
       </div>
     </div>
   );
